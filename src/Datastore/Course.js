ac3570128d934254d7b045d0e5ed62c078114344
Nitin Vamsi Dantu <41870240+nitindantu@users.noreply.github.com>
2022-01-14 21:17:11 +0530

    Add files via upload

---
+++ src/Datastore/Course.js
@@ -0,0 +1,94 @@
+export var course=[{
+    name:"Blockchain and Bitcoin Fundamentals",
+    image:"https://images.unsplash.com/photo-1626162953675-544bf5a61ca6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+description:"Learn the key elements of blockchain and Bitcoin in this bestselling video course and accompanying PDF glossary.",
+author:"Shriram K V",
+links:["https://youtu.be/RCHvt6KCcUk","https://youtu.be/a0izTK9tVvc","https://youtu.be/CGHHhcpx3x8","https://youtu.be/8QEPimlOX_8","https://youtu.be/egrp6YxN4Sc","https://youtu.be/8sDQPAeHeiU","https://youtu.be/DKclA3T5q6g"],
+month:"Dec",
+date:11,
+hrs:"42 hrs",
+sessions:"7"
+}
+,
+{
+    name:"Introduction to Accumulate Blockchain",
+    image:"https://docs.accumulatenetwork.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F-MhVaqCAmRpnPF2WUCP4%2Ficon%2FMsreRebfSBiK7OphVYAy%2FAccumulate%20logo%20-%20snowflake%20only.svg?alt=media&token=a44d779a-b364-4197-9dc0-8d2db98e93b5",
+description:"Learn how to use NumPy, Pandas, Seaborn , Matplotlib , Plotly , Scikit-Learn , Machine Learning, Tensorflow , and more!",
+author:"Jose Portilla",
+links:["https://youtu.be/RCHvt6KCcUk","https://youtu.be/a0izTK9tVvc","https://youtu.be/CGHHhcpx3x8","https://youtu.be/8QEPimlOX_8","https://youtu.be/egrp6YxN4Sc","https://youtu.be/8sDQPAeHeiU","https://youtu.be/DKclA3T5q6g"],
+month:"Jan",
+date:"04",
+hrs:"32 hrs",
+sessions:"7"
+
+},
+{
+    name:"Ethereum and Solidity: The Complete Developer's Guide",
+    image:"https://images.unsplash.com/photo-1626162987518-4fee900a9323?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+description:"Learn how to use NumPy, Pandas, Seaborn , Matplotlib , Plotly , Scikit-Learn , Machine Learning, Tensorflow , and more!",
+author:"Stephen Borsay",
+links:["https://youtu.be/RCHvt6KCcUk","https://youtu.be/a0izTK9tVvc","https://youtu.be/CGHHhcpx3x8","https://youtu.be/8QEPimlOX_8","https://youtu.be/egrp6YxN4Sc","https://youtu.be/8sDQPAeHeiU","https://youtu.be/DKclA3T5q6g"],
+month:"Jun",
+date:"22",
+hrs:"22 hrs",
+sessions:"3"
+
+},{
+    name:"Ethereum Blockchain Developer Bootcamp With Solidity (2022)",
+    image:"https://images.unsplash.com/photo-1627539002233-fc1846256163?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80",
+description:"Learn how to use NumPy, Pandas, Seaborn , Matplotlib , Plotly , Scikit-Learn , Machine Learning, Tensorflow , and more!",
+author:"Jose Portilla",
+links:["https://youtu.be/RCHvt6KCcUk","https://youtu.be/a0izTK9tVvc","https://youtu.be/CGHHhcpx3x8","https://youtu.be/8QEPimlOX_8","https://youtu.be/egrp6YxN4Sc","https://youtu.be/8sDQPAeHeiU","https://youtu.be/DKclA3T5q6g"],
+month:"Dec",
+date:"13",
+hrs:"12 hrs",
+sessions:"7"
+
+},{
+    name:"Blockchain for Business: The New Industrial Revolution",
+    image:"https://images.unsplash.com/photo-1559445368-b8a993676d7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2531&q=80",
+description:"Learn how to use NumPy, Pandas, Seaborn , Matplotlib , Plotly , Scikit-Learn , Machine Learning, Tensorflow , and more!",
+author:"Stefan",
+links:["https://youtu.be/RCHvt6KCcUk","https://youtu.be/a0izTK9tVvc","https://youtu.be/CGHHhcpx3x8","https://youtu.be/8QEPimlOX_8","https://youtu.be/egrp6YxN4Sc","https://youtu.be/8sDQPAeHeiU","https://youtu.be/DKclA3T5q6g"],
+month:"Aug",
+date:"12",
+hrs:"23 hrs",
+sessions:"7"
+
+},{
+    name:"Build your own Proof Of Stake Blockchain",
+    image:"https://media.istockphoto.com/photos/data-scientists-male-programmer-using-laptop-analyzing-and-developing-picture-id1295900106?b=1&k=20&m=1295900106&s=170667a&w=0&h=kQ2UWilU4Bild5aP03CaF65gMbSI-chG--7dd2oS8GM=",
+description:"Learn how to use NumPy, Pandas, Seaborn , Matplotlib , Plotly , Scikit-Learn , Machine Learning, Tensorflow , and more!",
+author:"Jose Portilla",
+links:["https://youtu.be/RCHvt6KCcUk","https://youtu.be/a0izTK9tVvc","https://youtu.be/CGHHhcpx3x8","https://youtu.be/8QEPimlOX_8","https://youtu.be/egrp6YxN4Sc","https://youtu.be/8sDQPAeHeiU","https://youtu.be/DKclA3T5q6g"],
+month:"Sep",
+date:"02",
+hrs:"45 hrs",
+sessions:"7"
+
+},{
+    name:"Introduction to Blockchain with Industry Applications",
+    image:"https://images.unsplash.com/photo-1582489853490-cd3a53eb4530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+description:"Learn how to use NumPy, Pandas, Seaborn , Matplotlib , Plotly , Scikit-Learn , Machine Learning, Tensorflow , and more!",
+author:"Klaus",
+links:["https://youtu.be/RCHvt6KCcUk","https://youtu.be/a0izTK9tVvc","https://youtu.be/CGHHhcpx3x8","https://youtu.be/8QEPimlOX_8","https://youtu.be/egrp6YxN4Sc","https://youtu.be/8sDQPAeHeiU","https://youtu.be/DKclA3T5q6g"],
+month:"Oct",
+date:"15",
+hrs:"38 hrs",
+sessions:"7"
+
+},{
+    name:"Cryptography",
+    image:"https://images.unsplash.com/photo-1597781914467-a5b93258e748?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZW5jcnlwdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
+description:"Learn how to use NumPy, Pandas, Seaborn , Matplotlib , Plotly , Scikit-Learn , Machine Learning, Tensorflow , and more!",
+author:"Jose Portilla",
+links:["https://youtu.be/RCHvt6KCcUk","https://youtu.be/a0izTK9tVvc","https://youtu.be/CGHHhcpx3x8","https://youtu.be/8QEPimlOX_8","https://youtu.be/egrp6YxN4Sc","https://youtu.be/8sDQPAeHeiU","https://youtu.be/DKclA3T5q6g"],
+month:"Apr",
+date:"22",
+hrs:"20 hrs",
+sessions:"7"
+
+}
+
+
+]
---
+++ src/Datastore/Mentor.js
@@ -0,0 +1,88 @@
+export var mentor=[
+    {
+        "name":"Shriram K V",
+        "about":"Shriram K. Vasudevan is an M.Tech., M.B.A Ph.D with over 11 years of industrial and academic experience. He received his master’s degree and Doctorate in the field of Embedded Systems. He has worked with major multinational companies like Wipro and Aricent Technologies; and been to the USA on a technical assignment while functioning for WIPRO technologies. He was also closely associated with VIT University. Presently, he is employed with Amrita School of Engineering, Coimbatore and serves as an Assistant Professor. He has authored/co-authored 32 books in various fields and is currently authoring 3 more books. ",
+        "image":"https://speakers.acm.org/binaries/content/gallery/speakers/photo/u-z/vasudevan_13509",
+    "workplace":"Massachusetts Institute of Technology",
+    "Experience":"5",
+    coffer:["Blockchain","Web design","Machine learning","IOT","Python"],
+    "timeslot":{
+    "Blockchain":["9.00 am - 10.00am","04.00 am - 05.00 am"],
+        "Web design":["10.00 am - 11.00am","03.00 am - 04.00 am"],
+        "Machine learning":["11.30 am - 12.30am","06.00 pm - 07.00 pm"],
+        "IOT":["12.30 am - 13.30am","02.00 am - 03.00 pm"]
+    
+    
+    }
+    }
+    ,
+    
+    {
+    "name":"Bonnie",
+    "about":"Bonnie has a BS and MS in Mechanical Engineering from Santa Clara University and years of experience as a professional instructor and trainer for Data Science and programming. He has publications and patents in various fields such as microfluidics, materials science, and data science technologies. ",
+    "image":"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80",
+"workplace":"Massachusetts Institute of Technology",
+"Experience":"5",
+coffer:["Blockchain","Web design","Machine learning","IOT"],
+"timeslot":{
+"Blockchain":["9.00 am - 10.00am","04.00 am - 05.00 am"],
+    "Web design":["10.00 am - 11.00am","03.00 am - 04.00 am"],
+    "Machine learning":["11.30 am - 12.30am","06.00 pm - 07.00 pm"],
+    "IOT":["12.30 am - 13.30am","02.00 am - 03.00 pm"]
+
+
+}
+}
+,
+{
+    name:"Damon",
+    about:"Damon has a BS and MS in Mechanical Engineering from Santa Clara University and years of experience as a professional instructor and trainer for Data Science and programming. He has publications and patents in various fields such as microfluidics, materials science, and data science technologies. ",
+    image:"https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80",
+workplace:"University of Oxford",
+Experience:"5",
+coffer:["Project Management","Web design","Artificial Intelligence","IOT"],
+timeslot:[
+    {"Project Management":["9.00 am - 10.00am","04.00 am - 05.00 am"]},
+    {"Web design":["10.00 am - 11.00am","03.00 am - 04.00 am"]},
+    {"Artificial Intelligence":["11.30 am - 12.30am","06.00 pm - 07.00 pm"]},
+    {"IOT":["12.30 am - 13.30am","02.00 am - 03.00 pm"]}
+
+],
+
+},
+{
+    name:"Alric",
+    about:"Alric has a BS and MS in Mechanical Engineering from Santa Clara University and years of experience as a professional instructor and trainer for Data Science and programming. He has publications and patents in various fields such as microfluidics, materials science, and data science technologies. ",
+
+    image:"https://images.unsplash.com/photo-1543965170-4c01a586684e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDZ8fG1hbnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+workplace:"Columbia University",
+Experience:"5",
+coffer:["Data Structures","Database Administrator","Machine learning","Semantic Web"],
+timeslot:[
+    {"Data Structures":["9.00 am - 10.00am","04.00 am - 05.00 am"]},
+    {"Database Administrator":["10.00 am - 11.00am","03.00 am - 04.00 am"]},
+    {"Machine learning":["11.30 am - 12.30am","06.00 pm - 07.00 pm"]},
+    {"Semantic Web":["12.30 am - 13.30am","02.00 am - 03.00 pm"]}
+
+],
+
+},
+{
+    name:"Elena",
+     about:"Elena has a BS and MS in Mechanical Engineering from Santa Clara University and years of experience as a professional instructor and trainer for Data Science and programming. He has publications and patents in various fields such as microfluidics, materials science, and data science technologies. ",
+
+    image:"https://images.unsplash.com/photo-1533993192821-2cce3a8267d1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHdvbWFuJTIwbW9kZXJufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+workplace:"University of Cambridge",
+Experience:"5",
+coffer:["Project Management","Web design","Artificial Intelligence","Algorithm and Analysis"],
+timeslot:[
+    {"Project Management":["9.00 am - 10.00am","04.00 am - 05.00 am"]},
+    {"Web design":["10.00 am - 11.00am","03.00 am - 04.00 am"]},
+    {"Artificial Intelligence":["11.30 am - 12.30am","06.00 pm - 07.00 pm"]},
+    {"Algorithm and Analysis":["12.30 am - 13.30am","02.00 am - 03.00 pm"]}
+
+],
+
+},
+
+]
